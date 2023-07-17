7d3fa8c2ae209bcb5a6d5c0a0030a5cebfc96280
elbedie <130108713+elbedie@users.noreply.github.com>
2023-07-17 09:01:32 -0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Stock History App
 
-Este aplicativo permite visualizar o histórico de ações de uma determinada empresa.
+Este aplicativo permite visualizar o histórico de ações de duas empresas diferentes.
 """
 
 import streamlit as st
@@ -12,26 +12,31 @@ import plotly.graph_objects as go
 # Titulo do app
 st.title('Stock History App')
 
-st.sidebar.title('Selecione o stock')
-ticker_symbol = st.sidebar.text_input('stock', 'AAPL', max_chars=10)
+st.sidebar.title('Selecione os stocks')
+ticker_symbol1 = st.sidebar.text_input('stock 1', 'AAPL', max_chars=10)
+ticker_symbol2 = st.sidebar.text_input('stock 2', 'GOOGL', max_chars=10)
 
 # baixando dados do yahoo
-data = yf.download(ticker_symbol, start = '2020-01-01', end = '2023-06-26')
+data1 = yf.download(ticker_symbol1, start='2020-01-01', end='2023-06-26')
+data2 = yf.download(ticker_symbol2, start='2020-01-01', end='2023-06-26')
 
 # exibir os dados
 st.subheader('Histórico')
-st.dataframe(data)
+st.dataframe(data1)
+st.dataframe(data2)
 
-# Exibir o arquivo
+# Exibir o gráfico de fechamento
 fig = go.Figure()
-fig.add_trace(go.Scatter(x = data.index, y = data['Close'], name ='Fechamento'))
-fig.update_layout(title = f"{ticker_symbol}", xaxis_title = 'Data', yaxis_title = 'Preço')
+fig.add_trace(go.Scatter(x=data1.index, y=data1['Close'], name=ticker_symbol1 + ' Fechamento'))
+fig.add_trace(go.Scatter(x=data2.index, y=data2['Close'], name=ticker_symbol2 + ' Fechamento'))
+fig.update_layout(title='Histórico de Fechamento', xaxis_title='Data', yaxis_title='Preço')
 
-# Exibir o gráfico
-st.plotly_chart(fig)
-
-#Exibir o gráfico de volume
+# Exibir o gráfico de volume
 fig_volume = go.Figure()
-fig_volume.add_trace(go.Scatter(x = data.index, y = data['Volume'], name = 'Volume'))
-fig_volume.update_layout(title = f"{ticker_symbol}", xaxis_title='Data', yaxis_title='Volume')
+fig_volume.add_trace(go.Scatter(x=data1.index, y=data1['Volume'], name=ticker_symbol1 + ' Volume'))
+fig_volume.add_trace(go.Scatter(x=data2.index, y=data2['Volume'], name=ticker_symbol2 + ' Volume'))
+fig_volume.update_layout(title='Histórico de Volume', xaxis_title='Data', yaxis_title='Volume')
+
+# Exibir os gráficos
+st.plotly_chart(fig)
 st.plotly_chart(fig_volume)
